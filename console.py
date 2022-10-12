7c7c1623b3eddda17588ba28c56914e932bfb6f3
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-12 01:24:40 -0700

    Console (command interpreter)

--- console.py
+++ console.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
+"""
+Imports:
+cmd: Library that provides a command line interface
+for an application.
+It has interactive she with comman completition and history
+"""
 import cmd
 
 
-
 class HBNBCommand(cmd.Cmd):
+    """Command Interpreter"""
     prompt = '(hbnb) '
     intro = "Type something!"
+
     def do_exit(self, inp):
         """Exit the application"""
         print("Bye")
         return True
 
     def help_exit(self):
+        """Provides help documentation for 'exit'"""
         print('exit the application. Shrothand: x q Ctrl-D.')
 
-    def do_add(self, inp):
-        print("Adding '{}'".format(inp))
-    
     def help_add(slef):
         print("Add a new entry to the system.")
-    
 
     def default(self, inp):
         if inp == 'x' or inp == 'q':
@@ -33,6 +37,6 @@ class HBNBCommand(cmd.Cmd):
     def do_add(self, inp):
         print("Adding '{}'".format(inp))
 
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
- 
