59218f0da53c5990c42369a604ef1ab322c24537
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-13 07:16:57 -0700

    Methode update

--- console.py
+++ console.py
@@ -57,6 +57,27 @@ class HBNBCommand(cmd.Cmd):
         except IndexError:
             print("** instance id missing **")
 
+    def do_destroy(self, inp):
+        checker = 0
+        dictionary = storage.all()
+        if len(inp) == 0:
+            print("** class name missing **")
+            return
+        parse = inp.split()
+        if parse[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        try:
+            for inp, value in dictionary.copy().items():
+                if inp == f"{parse[0]}.{parse[1]}":
+                    dictionary.pop(inp)
+                    storage.save()
+                    checker = 1
+            if checker == 0:
+                print("** no instance found **")
+        except IndexError:
+            print("** instance id missing **")
+
     def do_all(self, inp):
         parse = inp.split()
         if parse[0] not in self.classes:
@@ -70,9 +91,31 @@ class HBNBCommand(cmd.Cmd):
                     lst.append(str(val))
             print(lst)
 
-    def destroy(self, inp):
-        # To be continued
-        pass
+    def do_update(self, inp):
+        parse = inp.split()
+        if len(inp) == 0:
+            print("** class name missing **")
+            return False
+        if parse[0] not in self.classes:
+            print("** class doesn't exist **")
+            return False
+        if len(parse) == 1:
+            print("** instance id missing **")
+            return False
+        if len(parse) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(parse) == 3:
+            print("** value missing **")
+            print("----------DEBUGGING---------")
+            return False
+        if len(parse) == 4:
+            name = f"{parse[0]}.{parse[1]}"
+            inputType = type(parse[2])
+            var = parse[3]
+            var = var.strip('"')
+            setattr(storage.all()[name], parse[2], inputType(var))
+            storage.save()
 
 
 if __name__ == '__main__':
