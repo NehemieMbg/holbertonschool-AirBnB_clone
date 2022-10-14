55194e50e9dd35d3c5ec50d7847044f5d1c5efd6
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-14 05:48:49 -0700

    Update path

--- console.py
+++ console.py
@@ -8,12 +8,6 @@ It has interactive she with comman completition and history
 import cmd
 from models.base_model import BaseModel
 from models import storage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
