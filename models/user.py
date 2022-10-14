c91813ef7c98e97024fd974ce0eec380f8424a8e
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-14 00:44:41 -0700

    8-first User

--- models/user.py
+++ models/user.py
@@ -4,8 +4,7 @@ from base_model import BaseModel
 
 class User(BaseModel):
     """Class that inherits form BaseModel"""
-    def __init__(self):
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
