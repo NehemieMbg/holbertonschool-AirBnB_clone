09a97ac769f55a12e70a20fa40cf60a1fb20072e
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-14 05:52:27 -0700

    Test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,11 +2,6 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import amenity
-from models.review import Review
 
 
 class FileStorage:
