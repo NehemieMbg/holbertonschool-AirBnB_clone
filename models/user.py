5de9c0f9003f14e3083e35c43027413152baa763
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-14 00:41:58 -0700

    New methode

--- models/user.py
+++ models/user.py
@@ -4,7 +4,8 @@ from base_model import BaseModel
 
 class User(BaseModel):
     """Class that inherits form BaseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self):
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
